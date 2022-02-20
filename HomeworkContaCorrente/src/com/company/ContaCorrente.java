44798b82edea27652154f5ff8d4377104b2285fe
Pablokapp <pablokapp@gmail.com>
2022-02-19 23:25:36 -0300

    metodos corrigido

--- HomeworkContaCorrente/src/com/company/ContaCorrente.java
+++ HomeworkContaCorrente/src/com/company/ContaCorrente.java
@@ -3,52 +3,72 @@ package com.company;
 import org.w3c.dom.ls.LSOutput;
 
 public class ContaCorrente {
-    Cliente cliente[] = new Cliente[1];
+    Cliente cliente;
     String numeroConta;
     int agencia;
     double saldo = 0.0;
     double chequeEspecial = 0.0;
 
-    public ContaCorrente (Cliente cliente, String numeroConta, int agencia){
-        this.cliente[0] = cliente;
+    public ContaCorrente(Cliente cliente, String numeroConta, int agencia, double saldo, double chequeEspecial) {
+        this.cliente = cliente;
         this.numeroConta = numeroConta;
         this.agencia = agencia;
+        this.saldo = saldo;
+        this.chequeEspecial = chequeEspecial;
 
     }
 
 
     public void imprimirContaCorrente() {
-        System.out.println("o número da conta corrente é: "+ this.numeroConta);
+        System.out.println("o número da conta corrente é: " + this.numeroConta);
     }
 
     public boolean sacar(double valor) {
-        if (saldo < valor) {
-            return false;
-        } else if (saldo > valor)
-            return true;
 
-    }
-
-    public boolean depositar(double valor){
-
-
-
-        saldo += valor;
-
-
-        return true;
-    }
-
-    public double retornarSaldoComChequeEspecial(){
-        return saldo+chequeEspecial;
-    }
-
-    public boolean transferir(ContaCorrente contaCorrente, double valor){
+       if(valor > 0){
+           if (valor < retornarSaldoComChequeEspecial()) {
+               if(valor < saldo){
+                   saldo -= valor;
+                   System.out.println("Saque autorizado");
+               } double valorMenosSaldo = valor-saldo;
+               chequeEspecial -= valorMenosSaldo;
+               System.out.println("você retirou seu saldo e "+valorMenosSaldo + " do cheque especial");
+               return true;
+           }
+       }else{
+           System.out.println("não é permitido sacar valor nulo ou negativo");
+       }
 
         return false;
-
     }
 
+
+
+    public boolean depositar(double valor) {
+        if (valor > 0) {
+            saldo += valor;
+            System.out.println("deposito concluído");
+            return true;
+        } else {
+            System.out.println("é preciso informar um valor válido");
+        }
+        return false;
+    }
+
+    public double retornarSaldoComChequeEspecial() {
+        return saldo + chequeEspecial;
+    }
+
+    public boolean transferir(ContaCorrente contaCorrente, double valor) {
+            if(this.saldo > valor){
+                sacar(valor);
+                contaCorrente.depositar(valor);
+                System.out.println("transferência concluída");
+                return true;
+            }
+        System.out.println("saldo insuficiente");
+        return false;
+    }
 
 }
 
--- HomeworkContaCorrente/src/com/company/Main.java
+++ HomeworkContaCorrente/src/com/company/Main.java
@@ -11,24 +11,28 @@ public class Main {
                 "Brasil", 2, "rua 1", 200, "casa", "91845-57", "Alvorada",
                 "RS", "Brasil");
 
-        Cliente jose = new Cliente();
 
-        Cliente william = new Cliente("Anderson", "4545667", "telefone 1", "113123", 1,
-                "contato2", "12312313213", 2, 1,
-                "Júlio de Castilhos", 596, "ap 1", "90030-10", "Porto Alegre", "RS",
-                "Brasil", 2, "rua 1", 200, "casa", "91845-57", "Alvorada",
-                "RS", "Brasil");
-//        anderson.imprimirContatos();
-//        System.out.println("\n");
+
+//        Cliente william = new Cliente("Anderson", "4545667", "telefone 1", "113123", 1,
+//                "contato2", "12312313213", 2, 1,
+//                "Júlio de Castilhos", 596, "ap 1", "90030-10", "Porto Alegre", "RS",
+//                "Brasil", 2, "rua 1", 200, "casa", "91845-57", "Alvorada",
+//                "RS", "Brasil");
+////        anderson.imprimirContatos();
+////        System.out.println("\n");
 //
-        ContaCorrente teste = new ContaCorrente(anderson, "123134", 1);
+        ContaCorrente teste = new ContaCorrente(anderson, "123134", 1,1000,2000);
+        ContaCorrente test2 = new ContaCorrente(anderson, "123134", 1,1000,2000);
 
 
-        anderson.imprimirEndereco();
 
-        anderson.imprimirCliente();
-        teste.imprimirContaCorrente();
+//        anderson.imprimirEndereco();
+//        teste.transferir(teste, 100);
+//        anderson.imprimirCliente();
+//        teste.imprimirContaCorrente();
 
+//        teste.sacar(500);
+        teste.transferir(test2, 500);
 
     }
 }
