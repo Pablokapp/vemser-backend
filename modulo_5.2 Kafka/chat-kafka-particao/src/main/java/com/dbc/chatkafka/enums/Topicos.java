1e49dd3ac4194fa8ed9cfb39652e9d7454155d22
Pablokapp <pablokapp@gmail.com>
2022-04-13 15:03:28 -0300

    chat-kafka-particao

---
+++ modulo_5.2 Kafka/chat-kafka-particao/src/main/java/com/dbc/chatkafka/enums/Topicos.java
@@ -0,0 +1,36 @@
+package com.dbc.chatkafka.enums;
+
+
+public enum Topicos {
+    GERAL("chat-geral"),        // 0
+    ANA("chat-ana"),            // 1
+    EZEQUIAS("chat-ezequias"),  // 2
+    FLAVIO("chat-flavio"),
+    FELIPE("chat-felipe"),
+    GABRIEL("chat-gabriel"),
+    GUILHERME("chat-guilherme"),
+    GUSTAVO("chat-gustavo"),
+    JOAO("chat-joao"),
+    LUCAS("chat-lucas"),
+    LUIZ("chat-luiz"),
+    MAICON("chat-maicon"),      // 11
+    NICOLAS("chat-nicolas"),
+    PABLO("chat-pablo");
+
+    private String chatName;
+
+    Topicos(String chatName) {
+        this.chatName = chatName;
+    }
+
+    public String getChatName() {
+        return this.chatName;
+    }
+
+
+
+
+}
+
+
+
---
+++ modulo_5.2 Kafka/chat-kafka-particao/src/main/java/com/dbc/chatkafka/service/ConsumerService.java
@@ -0,0 +1,68 @@
+package com.dbc.chatkafka.service;
+
+import com.dbc.chatkafka.dto.ProdutorDTO;
+import com.dbc.chatkafka.enums.Topicos;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Service;
+
+import java.time.format.DateTimeFormatter;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class ConsumerService {
+
+    private final ObjectMapper objectMapper;
+    private final ProducerService producerService;
+
+
+    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+
+    @KafkaListener(
+            topicPartitions = @TopicPartition(topic = "${kafka.topaic.geral-chat}", partitions = {"0"}),
+            groupId = "${kafka.group-id}",
+            containerFactory = "listenerContainerFactory",
+            clientIdPrefix = "geral")
+    public void consumeChatGeral(@Payload String message,
+                        @Header(KafkaHeaders.RECEIVED_MESSAGE_KEY) String key,
+                        @Header(KafkaHeaders.OFFSET) Long offset) throws JsonProcessingException {
+        ProdutorDTO produtorDTO = objectMapper.readValue(message, ProdutorDTO.class);
+        String data = produtorDTO.getDataCriacao().format(formatter);
+        System.out.println(data + " [" + produtorDTO.getUsuario() + "] "  + produtorDTO.getMensagem());
+        log.info("#### offset -> '{}' key -> '{}' -> Consumed Object message -> '{}'  ", offset, key, message);
+    }
+
+    @KafkaListener(
+            topics = "${kafka.topic.meu-chat}",
+            groupId = "${kafka.group-id}",
+            containerFactory = "listenerContainerFactory",
+            clientIdPrefix = "private")
+    public void consumeMyChat(@Payload String message,
+                        @Header(KafkaHeaders.RECEIVED_MESSAGE_KEY) String key,
+                        @Header(KafkaHeaders.OFFSET) Long offset) throws JsonProcessingException {
+        ProdutorDTO produtorDTO = objectMapper.readValue(message, ProdutorDTO.class);
+        String data = produtorDTO.getDataCriacao().format(formatter);
+
+
+        if (!produtorDTO.getUsuario().equals("${kafka.client-id}")) {
+            producerService.sendReturn(produtorDTO.getUsuario());
+        }
+
+
+        System.out.println(data + " [" + produtorDTO.getUsuario() + "] (privada): "  + produtorDTO.getMensagem());
+        log.info("#### offset -> '{}' key -> '{}' -> Consumed Object message -> '{}'  ", offset, key, message);
+    }
+}
